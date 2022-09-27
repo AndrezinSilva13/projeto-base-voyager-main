699f180b18a7f4ea493c21584668cce3a345ba3a
André G. Silva <100439637+AndrezinSilva13@users.noreply.github.com>
2022-09-26 22:03:23 -0300

    Update header.blade.php

--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -1,13 +1,13 @@
 <header class="masthead">
     <div class="container">
         <div class="masthead-subheading">
-           {{setting('Lojão das Utilidades')}}
+           {{setting('site.title')}}
         </div>
         <div class="masthead-heading text-uppercase">
-            {{setting('A melhor loja de Bela Vista - MS!')}}
+            {{setting('site.description')}}
         </div>
         <a class="btn btn-primary btn-xl text-uppercase" href="#services">
-            {{$botao ?? 'Conheça nosso site!'}}
+            {{$botao ?? 'Conheça nosso WebSite!'}}
         </a>
     </div>
 </header>
