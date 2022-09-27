ce8e9eba52fdbddcbe3463de07aadf7010ea301a
André G. Silva <100439637+AndrezinSilva13@users.noreply.github.com>
2022-09-26 22:21:25 -0300

    Update assets.blade.php

--- resources/views/templates/assets.blade.php
+++ resources/views/templates/assets.blade.php
@@ -1,10 +1,1 @@
-<section class="page-section">
-    <div class="container">
-        <div class="text-center">
-            <h2 class="section-heading text-uppercase">Templates</h2>
-            <h3 class="section-subheading text-muted">Variedades de Produtos</h3>
-        </div>
-        <ul class="list-group">
-            <li class="list-group-item"><a target="_blank" href="https://startbootstrap.com/?showPro=false&showAngular=false&showVue=false">startbootstrap</a></li>
-        </ul>
-    </section>
+
